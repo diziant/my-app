bd8c661126e979d1142851c2570c8b415e3444b9
WIN-MRCJC3UND1N\User <folomeev-egor@mail.ru>
2017-11-24 22:27:04 +0300

    Init commit

---
+++ src/App.js
@@ -0,0 +1,48 @@
+﻿import React, { Component } from 'react';
+import './App.css';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      term: '',
+      items: []
+    };
+  }
+
+  onChange = (event) => {
+    this.setState({
+      term: event.target.value
+    });
+  }
+
+  onSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      term: '',
+      items: [...this.state.items, this.state.term]
+    });
+  }
+
+  render() {
+    return (
+      <div className="container">
+	  <h1 className="todo-title" >Задачи</h1>
+        <form className="todo-form" onSubmit={this.onSubmit}>
+          <input className="todo-form__input" placeholder="Добавьте задачу" value={this.state.term} onChange={this.onChange} />
+          <button className="todo-form__button">Запись</button>
+        </form>
+        <List items={this.state.items} />
+      </div>
+    );
+  }
+}
+
+const List = props => (
+  <ol className="todo-list">
+    {
+      props.items.map((item, index) => <li onClick={() => alert('Удаляйся')} className="todo-list__item" key={index}>{item}</li>)
+    }
+  </ol>
+);
