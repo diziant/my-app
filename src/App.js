9d359b7ff8ec566cf94147b7039ee3140caa86a5
DOC1-ПК\DOC1 <folomeev-egor@mail.ru>
2017-12-01 16:35:39 +0400

    add remove effect

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ export default class App extends Component {
       <div className="container">
 	  <h1 className="todo-title" >Задачи</h1>
         <form className="todo-form" onSubmit={this.onSubmit}>
-          <input className="todo-form__input" placeholder="Добавьте задачу" value={this.state.term} onChange={this.onChange} />
+          <input className="todo-form__input" placeholder="Добавьте задачу!" value={this.state.term} onChange={this.onChange} />
           <button className="todo-form__button">Запись</button>
         </form>
         <TodoList items={this.state.items} removeItem={this.removeItem}/>
