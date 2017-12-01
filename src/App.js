bfbaa06ecdd0b578c4787d5a929b019e499a33ee
DOC1-ПК\DOC1 <folomeev-egor@mail.ru>
2017-12-01 09:46:12 +0400

    check

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ export default class App extends Component {
       <div className="container">
 	  <h1 className="todo-title" >Задачи</h1>
         <form className="todo-form" onSubmit={this.onSubmit}>
-          <input className="todo-form__input" placeholder="Добавьте задачу" value={this.state.term} onChange={this.onChange} />
+          <input className="todo-form__input" placeholder="Добавьте задачу!" value={this.state.term} onChange={this.onChange} />
           <button className="todo-form__button">Запись</button>
         </form>
         <List items={this.state.items} />
