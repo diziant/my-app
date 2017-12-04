2ddb18e51095e425f135e47fe346e815d7a47686
DOC1-ПК\DOC1 <folomeev-egor@mail.ru>
2017-12-04 10:38:09 +0400

    add file structure

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from 'react';
 import './App.css';
-import List from './List';
+import List from './components/List';
 
 export default class App extends Component {
   constructor(props) {
