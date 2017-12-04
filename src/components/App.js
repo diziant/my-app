f3bac8eaf26275bce2d18a00cc7d5d9c869f47af
DOC1-ПК\DOC1 <folomeev-egor@mail.ru>
2017-12-04 12:19:26 +0400

    correct

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 ﻿import React, { Component } from 'react';
 import './App.css';
-import List from './components/List/List';
+import List from './List/List';
 
 export default class App extends Component {
   constructor(props) {
